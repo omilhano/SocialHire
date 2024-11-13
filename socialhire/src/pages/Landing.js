a23fc91a1f40e7ac40676570e47a276419e46b37
Alexandre Francisco <alexmvfrancisco@gmail.com>
2024-11-13 14:38:03 +0000

    progressing landingpage

--- socialhire/src/pages/Landing.js
+++ socialhire/src/pages/Landing.js
@@ -6,13 +6,13 @@ import Container from 'react-bootstrap/Container';
 const Landing = () => {
     const navigate = useNavigate();
     return (
-        <Container fluid id='background'>
+        <Container fluid id='background' className="g-0">
             <div className="landing-container">
                 <div className="landing-content">
                     <div className='landing-text'>
                         <p className="landing-description">
-                            Land your next opportunity through the power of personal recommendation
-                        </p>
+                            Land your next opportunity through the power of <span id='hightlight-teal'>personal recommendation
+                            </span></p>
                         <p className="landing-span">
                             Connect with your network to discover job opportunities and help others grow their careers, all based on trust and community.
                         </p>
@@ -25,6 +25,28 @@ const Landing = () => {
                             <button id='login' onClick={() => navigate('./SignIn')}>Log In</button>
                             <p className='decide'>OR</p>
                             <button id='joinus' onClick={() => navigate('./SignUp')}>Join Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='landing-highlight'>
+                    <div className='highlight-text'>
+                        <h4 className='highlight-title'>
+                            Users- Employee or Employer?
+                        </h4>
+                        <p className='highlight-description'>
+                            Wether you need some help moving furniture or taking care of your pets
+                            during vacation SocialHire helps you connect with someone that has these skills!
+                            Between your trusted friends find the ideal candidate for the gig.
+                        </p>
+                    </div>
+                    <div className='highlight-actions'>
+                        <p className='actions-text'>
+                            While being an user you can have two status.
+                            let's find out more about them.
+                        </p>
+                        <div className='actions-buttonsbox'>
+                            <button className='actions-button'>Employee</button>
+                            <button className='actions-button'>Employer</button>
                         </div>
                     </div>
                 </div>
