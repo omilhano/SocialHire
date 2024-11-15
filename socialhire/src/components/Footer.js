36ef371c2247fbe813441d051901f54b2fee5614
cyref <sofiacgodinho@gmail.com>
2024-11-15 18:29:58 +0000

    Update Footer.js

--- socialhire/src/components/Footer.js
+++ socialhire/src/components/Footer.js
@@ -3,23 +3,31 @@ import Container from 'react-bootstrap/Container';
 
 const Footer = () => {
   return (
-    <footer className="py-3" style={{ backgroundColor: "#17497B" }}>
+    <footer className="py-3" style={{ backgroundColor: "#E2E2E2" }}>
       <Container className="d-flex flex-column align-items-center">
-        <p id="trademarker-E5E5" style={{ color: "#E5E5E5", fontSize: "1.6em" }}>
+        <p id="trademarker-E5E5" style={{ color: "black", fontSize: "1.6em" }}>
           © {new Date().getFullYear()} SocialHire. All rights reserved.
         </p>
         <ul className="list-inline">
-          <li className="list-inline-item">
-            <a href="#" style={{ color: "#E5E5E5", fontSize: "1.2em", textDecoration: "none" }}>Privacy Policy</a>
+          <li className="list-inline-item mx-3">
+            <a href="/PrivacyPolicy" style={{ color: "black", fontSize: "1.2em", textDecoration: "none" }}>
+              Privacy Policy
+            </a>
           </li>
-          <li className="list-inline-item">
-            <a href="#" style={{ color: "#E5E5E5", fontSize: "1.2em", textDecoration: "none" }}>Terms of Service</a>
+          <li className="list-inline-item mx-3">
+            <a href="/TOS" style={{ color: "black", fontSize: "1.2em", textDecoration: "none" }}>
+              Terms of Service
+            </a>
           </li>
-          <li className="list-inline-item">
-            <a href="#" style={{ color: "#E5E5E5", fontSize: "1.2em", textDecoration: "none" }}>Contact Us</a>
+          <li className="list-inline-item mx-3">
+            <a href="/Contacts" style={{ color: "black", fontSize: "1.2em", textDecoration: "none" }}>
+              Contact Us
+            </a>
           </li>
-          <li className="list-inline-item">
-            <a href="#" style={{ color: "#E5E5E5", fontSize: "1.2em", textDecoration: "none" }}>Credits</a>
+          <li className="list-inline-item mx-3">
+            <a href="/Credits" style={{ color: "black", fontSize: "1.2em", textDecoration: "none" }}>
+              Credits
+            </a>
           </li>
         </ul>
       </Container>
@@ -28,4 +36,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
