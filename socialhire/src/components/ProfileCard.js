cc8578cbd8fa5c9a76238146f75b5db0ab8faaed
cyref <eligodinhoc@gmail.com>
2024-11-20 16:17:19 +0000

    profile card added

---
+++ socialhire/src/components/ProfileCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UnderConstruction from '../components/UnderConstruction'; // Adjust the path as needed
+
+const ProfileCard = () => {
+  return (
+    <div>
+      <UnderConstruction />
+    </div>
+  );
+};
+
+export default ProfileCard;
--- socialhire/src/pages/MainPage.js
+++ socialhire/src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import placeholderPic from '../images/placeholderPic.jpg';
 import '../styles/MainPage.css';
+import ProfileCard from '../components/ProfileCard';
 
 const Main = () => {
     return (
@@ -11,6 +12,7 @@ const Main = () => {
                 <div className="layout-sidebar">
                     <div className="sidebar-header">
                         <div className="header-top">
+                        <ProfileCard />
                             <div className="header-profile-pic">
                                 <img
                                     src={placeholderPic}
